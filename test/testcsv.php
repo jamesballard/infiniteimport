0bd83e6e65fa26f7ffe8619ce96a47ecfdbd0df7
Robert Egglestone <r.egglestone@auckland.ac.nz>
2012-12-16 15:05:06 +1300

    Add test scripts for load data local

---
+++ test/testcsv.php
@@ -0,0 +1,13 @@
+<?php
+
+$sql = "load data local infile 'testcsv.csv' into table csvtest";
+
+$db = new PDO('mysql:host=localhost;dbname=test', 'tester', '', array(
+	PDO::MYSQL_ATTR_LOCAL_INFILE => 1,
+	PDO::ATTR_PERSISTENT => true
+));
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$db->exec($sql);
+
+?>
