1842e1b2eb4f61f24f3664790454bae63d614515
Robert Egglestone <r.egglestone@auckland.ac.nz>
2012-12-23 17:11:17 +1300

    Tidy up test cases, fix issue with table locks lingering after failure

--- common.php
+++ common.php
@@ -11,8 +11,8 @@ function database() {
         $cfg = database_config();
 	$db_url = "mysql:host=" . $cfg['host'] . ";dbname=" . $cfg['database'];
 	$db = new PDO($db_url, $cfg['login'], $cfg['password'], array(
-		PDO::ATTR_PERSISTENT => true,
-		PDO::MYSQL_ATTR_LOCAL_INFILE => 1 /* Won't work on some versions of PHP, may need to custom compile */
+		//PDO::ATTR_PERSISTENT => true, // have to worry about connection state
+		PDO::MYSQL_ATTR_LOCAL_INFILE => 1 // Won't work on some versions of PHP, may need to custom compile
 	));
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	return $db;
