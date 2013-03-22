a55bdacf9312e650a048aa8910c41db91804604f
Robert Egglestone <r.egglestone@auckland.ac.nz>
2013-03-23 11:31:32 +1300

    Add schema changes for modules

--- schema-changes.sql
+++ schema-changes.sql
@@ -6,3 +6,6 @@ ALTER TABLE users ADD COLUMN dob date DEFAULT NULL AFTER name;
 ALTER TABLE users ADD COLUMN gender enum('M','F') DEFAULT NULL AFTER dob;
 ALTER TABLE users ADD CONSTRAINT FK_users_systems FOREIGN KEY (system_id) REFERENCES systems (id);
 ALTER TABLE users ADD UNIQUE INDEX system_sysid_ix (system_id, sysid);
+
+ALTER TABLE modules ADD COLUMN created DATETIME NULL;
+ALTER TABLE modules ADD COLUMN modified DATETIME NULL AFTER created;
