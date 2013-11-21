ff6d1ed02eea7ff8938a9dd1a806b7f5b79c7263
Robert Egglestone <r.egglestone@auckland.ac.nz>
2013-11-21 22:13:35 +1300

    Add support for user.idnumber

--- import/user.php
+++ import/user.php
@@ -3,6 +3,7 @@
 /*
 sysid: string, required, private id within source system
 username: string, optional, public id within source system
+idnumber: string, optional, institutional id for the person
 name: string, optional, what the person is known as
 gender: 'M' or 'F', optional
 dob: yyyy-mm-dd, optional
@@ -12,7 +13,7 @@ require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'common.php';
 
 $parser = new CsvIterator('php://input');
 $parser->setRequiredFields(array('sysid'));
-$parser->setOptionalFields(array('username', 'name', 'gender', 'dob'));
+$parser->setOptionalFields(array('username', 'idnumber', 'name', 'gender', 'dob'));
 
 $importer = new BulkImport($parser, 'users');
 $importer->setSystemSpecific(true);
