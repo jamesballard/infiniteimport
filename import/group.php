48c475d44f6e724c0ad26d1f6a12d9afbd199f99
Robert Egglestone <r.egglestone@auckland.ac.nz>
2013-11-21 22:23:54 +1300

    Add support for group.idnumber

--- import/group.php
+++ import/group.php
@@ -3,13 +3,14 @@
 /*
 sysid: string, required, private id within source system
 name: string, optional, what the group is known as
+idnumber: string, optional, institutional id for the group
 */
 
 require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'common.php';
 
 $parser = new CsvIterator('php://input');
 $parser->setRequiredFields(array('sysid'));
-$parser->setOptionalFields(array('name'));
+$parser->setOptionalFields(array('name', 'idnumber'));
 
 $importer = new BulkImport($parser, 'groups');
 $importer->setSystemSpecific(true);
