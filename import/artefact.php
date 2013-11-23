b43938c0d4a2e035c00ebebfbc32103dd971037d
Robert Egglestone <r.egglestone@auckland.ac.nz>
2013-11-23 15:44:39 +1300

    Artefact should be generic not system specific

--- import/artefact.php
+++ import/artefact.php
@@ -9,11 +9,9 @@ idnumber: string, optional, institutional id for the artefact
 require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'common.php';
 
 $parser = new CsvIterator('php://input');
-$parser->setRequiredFields(array('sysid'));
-$parser->setOptionalFields(array('name', 'idnumber'));
+$parser->setRequiredFields(array('name'));
 
 $importer = new BulkImport($parser, 'artefacts');
-$importer->setSystemSpecific(true);
 $importer->setDated(true);
 $importer->run();
 
