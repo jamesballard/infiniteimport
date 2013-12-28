772ebfc2dce13193d6f5ac64fd1caa63279572e9
Robert Egglestone <r.egglestone@auckland.ac.nz>
2013-12-29 10:47:55 +1300

    Add support for sysinfo on actions

--- import/action.php
+++ import/action.php
@@ -6,26 +6,28 @@ action: string, required, the action being performed, typically described by a v
 user: string, required, initiator of the action
 module: string, optional, affected module
 group: string, optional, affected group
+sysinfo: string, optional, system specific additional information
 */
 
 require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'common.php';
 
 $parser = new CsvIterator('php://input');
 $parser->setRequiredFields(array('time', 'action', 'user'));
-$parser->setOptionalFields(array('module', 'group'));
+$parser->setOptionalFields(array('module', 'group', 'sysinfo'));
 
 $translator = new CallbackMappingIterator($parser, function($key, $row) {
 	return array(
 		'time' => translate_date($row['time']),
 		'name' => $row['action'],
 		'user_id' => IdManager::fromApplication('users', $row['user']),
-		'module_id' => IdManager::fromApplication('modules', $row['module']),
-		'group_id' => IdManager::fromApplication('groups', $row['group']),
+		'module_id' => IdManager::fromApplication('modules', @$row['module']),
+		'group_id' => IdManager::fromApplication('groups', @$row['group']),
+		'sysinfo' => @$row['sysinfo'],
 	);
 });
 
 $importer = new BulkImport($translator, 'actions');
-$importer->setFields(array('time', 'name', 'user_id', 'module_id', 'group_id'));
+$importer->setFields(array('time', 'name', 'user_id', 'module_id', 'group_id', 'sysinfo'));
 $importer->setUpdate(false); # disallow updates, for performance
 $importer->setSystemSpecific(true);
 $importer->run();
