0f108121a501d13d3a3f8d503131d1a115269659
Robert Egglestone <r.egglestone@auckland.ac.nz>
2014-06-22 08:39:39 +1200

    Add support for user.email

--- import/user.php
+++ import/user.php
@@ -7,13 +7,14 @@ idnumber: string, optional, institutional id for the person
 name: string, optional, what the person is known as
 gender: 'M' or 'F', optional
 dob: yyyy-mm-dd, optional
+email, string , optional
 */
 
 require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'common.php';
 
 $parser = new CsvIterator('php://input');
 $parser->setRequiredFields(array('sysid'));
-$parser->setOptionalFields(array('username', 'idnumber', 'name', 'gender', 'dob'));
+$parser->setOptionalFields(array('username', 'idnumber', 'name', 'gender', 'dob', 'email'));
 
 $importer = new BulkImport($parser, 'users');
 $importer->setSystemSpecific(true);
