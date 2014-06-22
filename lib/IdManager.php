101a94d4458be252e192e846e6122b74b34bee91
Robert Egglestone <r.egglestone@auckland.ac.nz>
2014-06-22 19:17:55 +1200

    Add dated support to IdManager

--- lib/IdManager.php
+++ lib/IdManager.php
@@ -12,20 +12,25 @@ class IdManager {
 		$sysid_field = 'sysid';
 		if (array_key_exists('field', $options)) $sysid_field = $options['field'];
 		if (!is_array($sysid_field)) $sysid_field = array($sysid_field);
-		$sysid_field = '(' . implode(',', $sysid_field) . ')';
+		$sysid_field = implode(',', $sysid_field);
 		$sysid_var = preg_replace('/[a-z_]+/i', '?', $sysid_field);
 		
 		$create = false;
 		if (array_key_exists('create', $options)) $create = $options['create'];
 
+		$dated = false;
+		if (array_key_exists('dated', $options)) $dated = $options['dated'];
+
 		$key = IdManager::cacheKey($type, $sysid);
 		$id = apc_fetch($key);
 		if ($id === false) {
-			$id = query_value("select id from $type where $sysid_field = $sysid_var", $sysid);
+			$id = query_value("select id from $type where ($sysid_field) = ($sysid_var)", $sysid);
 			
 			if ($id === false && $create) {
-				sql_execute("insert into $type $sysid_field values $sysid_var", $sysid);
-				$id = query_value("select id from $type where $sysid_field = $sysid_var", $sysid);
+				$date_fields = $dated ? ',created' : '';
+				$date_vars = $dated ? ',now()' : '';
+				sql_execute("insert into $type ($sysid_field$date_fields) values ($sysid_var$date_vars)", $sysid);
+				$id = query_value("select id from $type where ($sysid_field) = ($sysid_var)", $sysid);
 			}
 			
 			if ($id === false) {
